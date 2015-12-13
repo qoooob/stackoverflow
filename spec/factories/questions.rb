ef1499b5c3e4142dc7f0b9f0ad67118c16c5e629
Aleksey Kubarev <aleksey.kubarev@gmail.com>
2015-12-13 23:34:39 +0300

    QuestionController with spec

--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -1,6 +1,13 @@
 FactoryGirl.define do
   factory :question do
-    
+
+    title "My question"
+    body "Question body"
+  end
+
+  factory :invalid_question, class: 'Question' do
+    title nil
+    body nil
   end
 
 end
